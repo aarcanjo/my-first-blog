6c2c1b2a3901271586cde5578b85e5f60a8c6386
Ana Arcanjo <a.arcanjo@gmail.com>
2018-06-09 17:55:54 -0300

    My Django Girls app, first commit

---
+++ python-intro.py
@@ -0,0 +1,13 @@
+def hi(nome):
+	print('Olá, '+nome+'!')
+		
+
+garotas=['Rachel', 'Monica', 'Phoebe', 'Ana']
+#for nome in garotas:
+#	hi(nome)
+#	print('Próxima garota')
+
+for i in range(1,6):
+	print(i)
+
+
